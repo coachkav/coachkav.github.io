58bcf69d571827cd8c6ef3c4eeea3efb9c5c8715
coachkav <37426091+coachkav@users.noreply.github.com>
2022-04-25 16:20:21 -0400

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -5,16 +5,16 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 
-USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
-GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
-BEST_GAME=$($PSQL "SELECT MIN(number_guess) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+USERNAME_I=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+BEST_G=$($PSQL "SELECT MIN(number_guess) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
 
-if [[ -z $USERNAME_AVAIL ]]
+if [[ -z $USERNAME ]]
 then
 INSERT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
 echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
-echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+echo "Welcome back, $USERNAME! You have played $GAMES games, and your best game took $BEST_G guesses."
 fi
 
 RANDOM_NUMBER=$((1 + $RANDOM % 1000))
@@ -42,12 +42,8 @@ while read NUM
 GUESS=$(( $GUESS + 1 ))
 done
 
-if [[ $GUESS == 1  ]]
-then
 echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUMBER. Nice job!"
-else
-echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUMBER. Nice job!"
-fi
+
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 INSERT_GAME=$($PSQL "INSERT INTO games(number_guess, user_id) VALUES($GUESS, $USER_ID)")
